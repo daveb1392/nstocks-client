ab7afc750ededecdb46080a98d16e6cfe25a0c66
github username <github email address>
2019-09-23 12:02:16 +0100

    added a temp table for now

--- src/components/Stocks.js
+++ src/components/Stocks.js
@@ -1,28 +1,76 @@
 import React from "react";
-
-class Stock extends React.Component{
-
-state = {
-  stock: []
-  
-  }
+import Table from "./Table";
 
 
 
-  render(){
-    return(
-  <div>
-    <div className="card" onClick={() => this.props.stockAction(this.props.stock)}>
-      <div className="card-body">
-        <h5 className="card-title">
-          {this.props.stock["Global Quote"]["01. symbol"]}
-        </h5>
-        <p className="card-text">{this.props.stock["Global Quote"]["05. price"]}</p>
-      </div>
-    </div>
-  </div>
-)  
 
+
+class Stock extends React.Component {
+  // do I need to have a state here because it will automatically update
+  state = {
+    stock: []
+  };
+
+  data = [
+    {
+      id: this.props.stock["Global Quote"]["01. symbol"],
+      name: this.props.stock["Global Quote"]["05. price"],
+      value: this.props.stock["Global Quote"]["09. change"]
+    }
+  ];
+
+  render() {
+    debugger;
+    return (
+      <div>
+        {/* <div
+          className="card"
+          onClick={() => this.props.stockAction(this.props.stock)}
+        >
+          <div className="card-body">
+            <h5 className="card-title">
+              {this.props.stock["Global Quote"]["01. symbol"]}
+            </h5>
+            <p className="card-text">
+              {this.props.stock["Global Quote"]["05. price"]}
+            </p>
+          </div> */}
+          <div className="App">
+            {/* <p className="Table-header"></p> */}
+            <Table data={this.data} />
+          </div>
+        </div>
+      // </div>
+    );
   }
 }
 export default Stock;
+
+// import React, { Component } from 'react';
+// import {BootstrapTable, 
+//        TableHeaderColumn} from 'react-bootstrap-table';
+// import '../css/Table.css';
+// import '../../node_modules/react-bootstrap-table/css/react-bootstrap-table.css'
+ 
+ 
+// class Table1 extends Component {
+//   render() {
+//     return (
+//       <div>
+//         <BootstrapTable data={this.props.data}>
+//           <TableHeaderColumn isKey dataField='id'>
+//             ID
+//           </TableHeaderColumn>
+//           <TableHeaderColumn dataField='name'>
+//             Name
+//           </TableHeaderColumn>
+//           <TableHeaderColumn dataField='value'>
+//             Value
+//           </TableHeaderColumn>
+//         </BootstrapTable>
+//       </div>
+//     );
+//   }
+// }
+ 
+// export default Table1;
